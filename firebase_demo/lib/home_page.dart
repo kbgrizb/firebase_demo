4e86b31e8161a2ec18ce72fcb628f9ad9a42ccd4
kbgrizb <grisbykb@hendrix.edu>
2024-10-03 19:18:51 -0500

    Steps 05/06 done
    forgot to commit 5

--- firebase_demo/lib/home_page.dart
+++ firebase_demo/lib/home_page.dart
@@ -2,9 +2,16 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart' 
+    hide EmailAuthProvider, PhoneAuthProvider;    
+import 'package:flutter/material.dart';           
+import 'package:provider/provider.dart';          
 
+import 'app_state.dart';                          
+import 'src/authentication.dart';                 
 import 'src/widgets.dart';
+import 'guest_book.dart';
+
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -21,16 +28,19 @@ class HomePage extends StatelessWidget {
           const SizedBox(height: 8),
           const IconAndDetail(Icons.calendar_today, 'October 30'),
           const IconAndDetail(Icons.location_city, 'San Francisco'),
-          const Divider(
-            height: 8,
-            thickness: 1,
-            indent: 8,
-            endIndent: 8,
-            color: Colors.grey,
-          ),
-          const Header("What we'll be doing"),
-          const Paragraph(
-            'Join us for a day full of Firebase Workshops and Pizza!',
+          Consumer<ApplicationState>(
+            builder: (context, appState, _) => Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                if (appState.loggedIn) ...[
+                  const Header('Discussion'),
+                  GuestBook(
+                    addMessage: (message) =>
+                        appState.addMessageToGuestBook(message),
+                  ),
+                ],
+              ],
+            ),
           ),
         ],
       ),
